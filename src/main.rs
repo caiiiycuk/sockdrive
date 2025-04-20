2656502eb125171460ce2e5157614835b2eb8a33
Alexander Guryanov <caiiiycuk@gmail.com>
2025-04-21 00:21:42 +0300

    Introduce default preload ranges

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ const AHEAD_READ_SIZE: u64 = 256 * 1024;
 const FAT16_256MB: &str = include_str!("../drives/fat16-256mb.json");
 const FAT32_2GB: &str = include_str!("../drives/fat32-2gb.json");
 const SMALL_FILES_THRESHOLD: u64 = 1024 * 1024;
+const DEFAULT_PRELOAD: &str = "0,16,1,52,50,68,51,145,280,152,291,227,234,226,207,279,7195,257,233,179,231,390,177,346,66,71,96,197,297,70,90,113,146,87,89,98,2,93,199,236,54,198,129,228,296,299,311,180,20,100,208,218,219,232,276,300,24,114,143,195,229,239,253,241,277,289,49,155,240,21,23,99,116,151,217,97,202,429,32,157,262,327,200,201,25,156,237,278,329,82,141,142,154,158,178,338,339,84,78,65,148,160,271,282,117,119,144,275,83,85,92,3,159,242,274,105,118,543,64,187,261,269,86,225,545,22,38,57,188,287,330,176,359,544,56,281,295,245,79,30,407,165,194,235,285,465,101,238,411,58,138,193,293,394,133,134,168,412,6,55,62,163,333,343,112,172,428,430,17,18,19,67,184,332,171,104,7,36,284,334,386,395,139,167,357,431,37,76,140,460,244,258,331,532,290,12,13,14,69,153,272,328,396,461,675,175,663,664,149,405,531,15,123,63,464,53,31,221,252,294,340,344,35,60,72,288,246,459,462,463,4,513,546,677,9,75,94,164,216,251,363,220,658,701,397,59,196,230,354,364,667,323,533,729";
 
 fn main() {
     task(std::env::args().collect());
@@ -27,7 +28,7 @@ Usage:
     -b: enable brotli compression (brotli cmd should be in PATH)
 
 Note 1:
-    you can use '_' as a preload sector to preload all ranges
+    use '_' as a default preload ranges
 
 Note 2:
     if you use qcow2 image then you must change permissions of /boot/vmlinuz-*
@@ -37,7 +38,7 @@ Note 2:
     https://askubuntu.com/questions/1046828/how-to-run-libguestfs-tools-tools-such-as-virt-make-fs-without-sudo
 
 Example:
-    sockdrive mkd win95v1.raw ./output [-b]
+    sockdrive mkd win95v1.raw _ ./output [-b]
         "
         );
         std::process::exit(1);
@@ -109,33 +110,18 @@ Example:
         .unwrap()
         .insert(String::from("dropped_ranges"), serde_json::json!(dropped));
 
-    let preload_ranges: Vec<u32> = if preload == "_" {
-        (0..range_count as u32).collect()
-    } else {
-        preload
+    let preload_ranges: Vec<u32> = 
+        if preload == "_" { DEFAULT_PRELOAD } else { preload }
             .split(',')
             .filter_map(|s| s.trim().parse().ok())
             .filter(|range| *range < range_count as u32)
-            .collect()
-    };
-
-    if preload == "_" {
-        config
-            .as_object_mut()
-            .unwrap()
-            .insert(String::from("preload_ranges"), serde_json::json!("_"));
-    } else {
-        let preload_ranges: Vec<u32> = preload_ranges
-            .iter()
             .filter(|range| !dropped.contains(range))
-            .copied()
             .collect();
 
-        config.as_object_mut().unwrap().insert(
-            String::from("preload_ranges"),
-            serde_json::json!(preload_ranges),
-        );
-    }
+    config.as_object_mut().unwrap().insert(
+        String::from("preload_ranges"),
+        serde_json::json!(preload_ranges),
+    );
 
     write(
         format!("{}/sockdrive.metaj", output_dir),
